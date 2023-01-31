92017ab75348e587455d71c85b34efa94494fde9
Bruno Henrique <brunodorea@outlook.com.br>
2023-01-31 10:55:48 -0300

    ✨ feat: criação do whiteboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import ReactFlow, { addEdge, Background, Connection, ConnectionMode, Controls, useEdgesState, useNodesState } from 'reactflow';
+import * as Toolbar from '@radix-ui/react-toolbar';
+import { zinc } from 'tailwindcss/colors'
+import 'reactflow/dist/style.css';
+
+import { Square } from './components/nodes/Square';
+import { useCallback } from 'react';
+import DefaultEdge from './components/edges/DefaultEdges';
+
+const NODE_TYPES = {
+  square: Square,
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 200
+    },
+    data: {},
+  },
+] satisfies Node[]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ edges, setEdges, onEdgesChange ] = useEdgesState([])
+  const [ nodes, setNodes, onNodesChange ] = useNodesState(INITIAL_NODES)
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges))
+  }, [])
+
+  function addSquareNode() {
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+          x: 750,
+          y: 350
+        },
+        data: {},
+      },
+    ])
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='w-screen h-screen'>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default',
+        }}
+      >
+        <Background 
+          gap={12}
+          size={2}
+          color={zinc[200]}
+        />
+        <Controls />
+      </ReactFlow>
+
+      <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+        <Toolbar.Button 
+          onClick={addSquareNode}
+          className='w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2' 
+        />
+      </Toolbar.Root>
     </div>
   )
 }
